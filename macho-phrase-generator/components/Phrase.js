e74ac73942bd0d0c48743ac53e7a9308eacc724f
Dominik Pömmer <dominik25664@gmail.com>
2022-03-02 20:52:51 +0100

    styling upgrade

--- macho-phrase-generator/components/Phrase.js
+++ macho-phrase-generator/components/Phrase.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Paper, Button, Typography } from '@mui/material'
+import { Paper, Typography } from '@mui/material'
 
-export default function Phrase({text}) {
-  return (
-
-      <Paper elevation={2} sx={{padding: 4}}>
-        <Typography variant="h1">Macho sagt:</Typography>
-        <Typography variant="h2">{text}</Typography>
-      </Paper>
-  )
+export default function Phrase({ text }) {
+    return (
+        <Paper elevation={4} sx={{ padding: 4 }}>
+            <Typography variant="h2">{text}</Typography>
+        </Paper>
+    )
 }
--- macho-phrase-generator/pages/index.js
+++ macho-phrase-generator/pages/index.js
@@ -1,67 +1,77 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Grid, IconButton } from '@mui/material'
+import { Grid, IconButton, Typography } from '@mui/material'
 import RefreshIcon from '@mui/icons-material/Refresh'
 import Phrase from '../components/Phrase'
 import { useEffect, useState } from 'react'
+import Button from '@mui/material/Button'
 
 export default function Home() {
 
-  const [worddata, setWorddata] = useState('')
-  const [phrase, setPhrase] = useState('')
+    const [worddata, setWorddata] = useState('')
+    const [phrase, setPhrase] = useState('')
 
-  useEffect(async () => {
-    const response = await fetch("./worddata.json")
-    const jsonData = response.json()
+    useEffect(async () => {
+        const response = await fetch("./worddata.json")
+        const jsonData = await response.json()
 
-    setWorddata(jsonData)
-  }, [])
+        setWorddata(jsonData)
+    }, [])
 
-  // won si wordsdata ändert, also nur am anfang beim einlesen, generier ma a neiche phrase
-  useEffect(() => {
-    generatePhrase()
-  }, [worddata])
+    // won si wordsdata ändert, also nur am anfang beim einlesen, generier ma a neiche phrase
+    useEffect(() => {
+        generatePhrase()
+    }, [worddata])
 
 
-  function generatePhrase() {
+    function generatePhrase() {
+        const phrases = worddata.phrases
+        const nouns = worddata.nouns
 
-    const phrases = worddata.phrases
-    const nouns = worddata.nouns
+        if (!phrases || !nouns) {
+            console.log('Error loading phrases and nouns')
+            console.log('worddata:', worddata)
+            console.log('phrases:', phrases)
+            console.log('nouns:', nouns)
+            return
+        }
 
-    if (!phrases || !nouns) {
-      console.log('Error loading phrases and nouns')
-      console.log('worddata:', worddata)
-      console.log('phrases:', phrases)
-      console.log('nouns:', nouns)
-      return
+        console.log(phrases)
+
+        const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)]
+        console.log(randomPhrase)
+        let phrase = 'test'
+
+        setPhrase(randomPhrase)
     }
 
-    console.log(phrases)
-    
-    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)]
-    console.log(randomPhrase)
-    let phrase = 'test'
-    
-    setPhrase(phrase)
-  }
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Macho-Sprüche</title>
+                <meta name="description" content="Macho-Sprüche Generator" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/* Header */}
+            <Grid container width="100%" alignItems="center" justifyContent="space-between" marginBottom={4}>
+                <Grid item>
+                    <Typography variant="h1">Macho sagt:</Typography>
+                </Grid>
+                <Grid item justifySelf="flex-end">
+                    <IconButton onClick={generatePhrase}>
+                        <RefreshIcon />
+                    </IconButton>
+                </Grid>
+            </Grid>
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Macho-Sprüche</title>
-        <meta name="description" content="Macho-Sprüche Generator" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Grid container width="100%" direction="column" justifyContent="center">
-        <Grid item>
-          <Phrase text={phrase}></Phrase>
-        </Grid>
-        <Grid item>
-          <IconButton onClick={generatePhrase}>
-            <RefreshIcon />
-          </IconButton>
-        </Grid>
-      </Grid>
-    </div>
-  )
+            {/* Phrase Container */}
+            <Grid container width="100%" direction="column" alignItems="center">
+                <Grid item width="80%">
+                    <Button onClick={generatePhrase} sx={{textTransform: 'none'}}>
+                        <Phrase text={phrase}></Phrase>
+                    </Button>
+                </Grid>
+            </Grid>
+        </div>
+    )
 }
