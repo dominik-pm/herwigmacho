077f5092274194788dd108da751f4a4419cda057
Dominik Pömmer <46850095+dominik-pm@users.noreply.github.com>
2022-03-10 13:45:39 +0100

    Update Phrase.js

--- macho-phrase-generator/components/Phrase.js
+++ macho-phrase-generator/components/Phrase.js
@@ -5,7 +5,7 @@ export default function Phrase({ text, maxCount }) {
     return (
         <Paper elevation={4} sx={{ padding: 4 }}>
             <Typography variant="h3">{text}</Typography>
-            <Typography variant="body1">1/{maxCount}</Typography>
+            {/*<Typography variant="body1">1/{maxCount}</Typography>*/}
         </Paper>
     )
 }
